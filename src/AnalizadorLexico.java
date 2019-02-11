1c4c43ed07926064731a9fc0b5b2bf2a43986203
Alvox12 <urda.alvaro@gmail.com>
2019-02-11 13:51:43 +0100

    codigo casi acabado

--- src/AnalizadorLexico.java
+++ src/AnalizadorLexico.java
@@ -1,23 +1,26 @@
 import java.io.IOException;
 import java.io.Reader;
 
+import com.sun.source.tree.WhileLoopTree;
+
 public class AnalizadorLexico {
 
 	private Reader input; // flujo de entrada
 	private StringBuffer lexema; // cadena para almacenar el lexema de la
-	// u. léxica
-	private int sigCar; // siguiente carácter a procesar
-	private int filaInicio; // fila inicio de la u. léxica
-	private int columnaInicio; // col. inicio de la u. léxica
+	// u. lï¿½xica
+	private int sigCar; // siguiente carï¿½cter a procesar
+	private int filaInicio; // fila inicio de la u. lï¿½xica
+	private int columnaInicio; // col. inicio de la u. lï¿½xica
 	private int filaActual;
 	private int columnaActual;
-	private Estado estado; // estado del autómata
+	private Estado estado; // estado del autï¿½mata
+	private static String NL = System.getProperty("line.separator");
 	
 	
 	enum Estado {
-		INICIO, REC_POR, REC_DIV, REC_PAP, REC_PCIERR, REC_COMA, REC_IGUAL,
-		REC_MAS, REC_MENOS, REC_ID, REC_ENT, REC_0, REC_IDEC, REC_DEC,
-		REC_COM,REC_EOF
+		INICIO, REC_POR, REC_DIV, REC_PAP, REC_PCIERR, REC_PUNTOCOMA, REC_PUNTO, REC_IGUAL, REC_EQUIVALENTE,
+		REC_MAYOR, REC_MENOR, REC_MAYIGUAL, REC_MENIGUAL, REC_MAS, REC_MENOS, REC_ID, REC_ENT, 
+		REC_DEC, REC_EXP, REC_SEPARADOR, REC_DIFERENTE, REC_EOF
 	}
 	
 	public AnalizadorLexico(Reader input) throws IOException {
@@ -30,39 +33,274 @@ public class AnalizadorLexico {
 
 	public UnidadLexica sigToken() throws IOException {
 		
-		// Fase de inicialización
+		// Fase de inicializaciï¿½n
 		estado = Estado.INICIO;
 		filaInicio = filaActual;
 		columnaInicio = columnaActual;
 		lexema.delete(0,lexema.length());
-		
-		return null;
+		while(true) {
+			switch(estado) {
+				case INICIO: 
+					if(hayLetra()) transita(Estado.REC_ID);
+					else if(hayDigito()) transita(Estado.REC_ENT);
+					else if (haySuma()) transita(Estado.REC_MAS);
+					else if (hayResta()) transita(Estado.REC_MENOS);
+					else if (hayMul()) transita(Estado.REC_POR);
+					else if (hayDiv()) transita(Estado.REC_DIV);
+					else if (hayPAp()) transita(Estado.REC_PAP);
+					else if (hayPCierre()) transita(Estado.REC_PCIERR);
+					else if (hayIgual()) transita(Estado.REC_IGUAL);
+					else if (hayMayor()) transita(Estado.REC_MAYOR);
+					else if (hayMenor()) transita(Estado.REC_MENOR);
+					else if (hayPunto()) transita(Estado.REC_PUNTO);
+					else if (hayPuntoComa()) transita(Estado.REC_PUNTOCOMA);
+					else if (haySep()) transitaIgnorando(Estado.INICIO);
+             	 	else if (hayEOF()) transita(Estado.REC_EOF);
+             	 	else if (haySeparador()) transita(Estado.REC_SEPARADOR);
+             	 	else if(hayExclamacion()) transita(Estado.REC_DIFERENTE);
+              		else error();
+					break;
+				case REC_ID:
+					if (hayLetra() || hayDigito() || hayBarrabaja()) transita(Estado.REC_ID);
+		            else return unidadId();     
+					break;
+				case REC_ENT:
+					if (hayDigito()) transita(Estado.REC_ENT);
+		            else if (hayPunto()) transita(Estado.REC_DEC);
+		            else if(hayExponente()) transita(Estado.REC_EXP);
+		            else return unidadEnt();
+		            break;
+				case REC_DEC: 
+		               if (hayDigito()) transita(Estado.REC_DEC);
+		               else if(hayExponente()) transita(Estado.REC_EXP);
+		               else return unidadReal();
+		               break;
+				case REC_EXP:
+					if(haySuma()) transita(Estado.REC_MAS);
+					else if(hayResta()) transita(Estado.REC_MENOS);
+					else if(hayDigito()) transita(Estado.REC_ENT);
+					break;
+				case REC_MAS:
+		            if (hayDigito()) transita(Estado.REC_ENT);
+		            else return unidadMas();
+		            break;
+				case REC_MENOS: 
+		            if (hayDigito()) transita(Estado.REC_ENT);
+		            else return unidadMenos();
+		            break;
+				case REC_MAYOR: 
+					if(hayIgual()) transita(Estado.REC_MAYIGUAL);
+					else return unidadMayor();
+					break;
+				case REC_MENOR: 
+					if(hayIgual()) transita(Estado.REC_MENIGUAL);
+					else return unidadMenor();
+					break;
+				case REC_MAYIGUAL: return unidadMayorIgual();
+				case REC_MENIGUAL: return unidadMenorIgual();
+				case REC_POR: return unidadPor();
+		        case REC_DIV: return unidadDiv();              
+		        case REC_PAP: return unidadPAp();
+		        case REC_PCIERR: return unidadPCierre();
+		        case REC_IGUAL: 
+		        	if(hayIgual()) transita(Estado.REC_EQUIVALENTE);
+		        	else return unidadIgual();
+		        	break;
+		        case REC_EQUIVALENTE: 
+		        	return unidadEquivalente();
+		        case REC_DIFERENTE:
+		        	return unidadDiferente();
+				case REC_SEPARADOR:
+					return unidadSeparador();
+				case REC_PUNTO:
+					return unidadPunto();
+				case REC_PUNTOCOMA:
+					return unidadPuntoComa();
+				case REC_EOF: return unidadEof();
+			}
+		}
+
 	}
 	
+	private void transita(Estado sig) throws IOException {
+		lexema.append((char) sigCar);
+		sigCar();
+		estado = sig;
+	}
+
+	private void transitaIgnorando(Estado sig) throws IOException {
+		sigCar();
+		filaInicio = filaActual;
+		columnaInicio = columnaActual;
+		estado = sig;
+	}
+
+	private void sigCar() throws IOException {
+		sigCar = input.read();
+		if (sigCar == NL.charAt(0))
+			saltaFinDeLinea();
+		if (sigCar == '\n') {
+			filaActual++;
+			columnaActual = 0;
+		} else {
+			columnaActual++;
+		}
+	}
+
+	private void saltaFinDeLinea() throws IOException {
+		for (int i = 1; i < NL.length(); i++) {
+			sigCar = input.read();
+			if (sigCar != NL.charAt(i))
+				error();
+		}
+		sigCar = '\n';
+	}
+
 	private boolean hayLetra() {
 		return sigCar >= 'a' && sigCar <= 'z' ||
 		sigCar >= 'A' && sigCar <= 'z';
 	}
 	
-	private boolean hayCero() {
-		return sigCar == 0;
+	private boolean haySeparador() {
+		return sigCar == '&'; 
 	}
 	
-	private boolean hayDigitoPos() {
-		return sigCar >= 1 && sigCar <= 9;
+	private boolean hayExclamacion() {
+		return sigCar == '!';
 	}
 	
 	private boolean hayDigito() {
-		return hayDigitoPos()||hayCero();
+		return sigCar >= '0' && sigCar <= '9';
 	}
+	
+	private boolean haySuma() {
+		return sigCar == '+';
+	}
+	
+	private boolean hayResta() {
+		return sigCar == '-';
+	}
+	
+	private boolean hayMul() {return sigCar == '*';}
+	
+	private boolean hayDiv() {return sigCar == '/';}
+	
+	private boolean hayPAp() {return sigCar == '(';}
+	
+	private boolean hayPCierre() {return sigCar == ')';}
+	
+	private boolean hayIgual() {return sigCar == '=';}
+	
+	private boolean hayMayor() {return sigCar == '>';}
+	
+	private boolean hayMenor() {return sigCar == '<';}
 	
 	private boolean hayPunto() {
 		return sigCar == '.';
 	}
-	
-	private boolean hayAlmohadilla() {
-		return sigCar == '#';
+
+	private boolean hayPuntoComa() {
+		return sigCar == ';';
 	}
+	
+	private boolean hayBarrabaja() {
+		return sigCar == '_';
+	}
+
+	private boolean haySep() {
+		return sigCar == ' ' || sigCar == '\t' || sigCar == '\n';
+	}
+	
+	private boolean hayExponente() {
+		return sigCar == 'e'||sigCar == 'E';
+	}
+	
+	private boolean hayEOF() {return sigCar == -1;}
+	
+	private UnidadLexica unidadId() {
+	     switch(lexema.toString()) {
+	         case "num":  
+	            return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.NUM);
+	         case "bool":    
+	            return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.BOOL);
+	         case "or": 
+	        	return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.OR);
+	         case "and": 
+	        	return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.AND);
+	         case "not": 
+	        	return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.NOT);
+	         default:    
+	            return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.IDEN,lexema.toString());     
+	      }
+	   } 
+	
+	private UnidadLexica unidadEnt() {
+	     return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.ENT,lexema.toString());     
+	}
+	
+	private UnidadLexica unidadReal() {
+	     return new UnidadLexicaMultivaluada(filaInicio,columnaInicio,ClaseLexica.REAL,lexema.toString());     
+	}
+	
+	private UnidadLexica unidadSeparador() {
+		return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.SEPARADOR);
+	}
+	
+	private UnidadLexica unidadDiferente() {
+		return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.DIFERENTE);
+	}
+	
+	private UnidadLexica unidadMas() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MAS);     
+	}    
+	private UnidadLexica unidadMenos() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MENOS);     
+	}    
+	private UnidadLexica unidadPor() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.POR);     
+	}    
+	private UnidadLexica unidadDiv() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.DIV);     
+	}    
+	private UnidadLexica unidadPAp() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.PAP);     
+	}    
+	private UnidadLexica unidadPCierre() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.PCIERRE);     
+	}
+	private UnidadLexica unidadMayor() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MAYOR);     
+	}
+	private UnidadLexica unidadMenor() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MENOR);     
+	}
+	private UnidadLexica unidadMayorIgual() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MAYIGUAL);     
+	} 
+	private UnidadLexica unidadMenorIgual() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.MENIGUAL);     
+	} 
+	private UnidadLexica unidadIgual() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.IGUAL);     
+	}
+	private UnidadLexica unidadEquivalente() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.EQUIVALENTE);     
+	}
+	private UnidadLexica unidadPuntoComa() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.PUNTOCOMA);     
+	}
+	private UnidadLexica unidadPunto() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.PUNTO);     
+	}
+	private UnidadLexica unidadEof() {
+	     return new UnidadLexicaUnivaluada(filaInicio,columnaInicio,ClaseLexica.EOF);     
+	}
+
+	private void error() {
+		System.err.println("(" + filaActual + ',' + columnaActual + "):Caracter inexperado");
+		System.exit(1);
+	}
+
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Reader input = null;
-		try {
-			AnalizadorLexico analizador = new AnalizadorLexico(input);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+	public static void main(String[] args) throws IOException {
+		Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+		AnalizadorLexico al = new AnalizadorLexico(input);
+		UnidadLexica unidad;
+		do {
+			unidad = al.sigToken();
+			System.out.println(unidad);
+		} while (unidad.clase() != ClaseLexica.EOF);
 	}
 
 }
